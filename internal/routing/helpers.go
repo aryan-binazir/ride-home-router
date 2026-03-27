fa6bd63c723bf4f29a7eeeb324f1a03a88eaa3a5
Aryan Binazir <abinazir@gmail.com>
2026-03-27 09:14:06 -0400

    lint fix

--- internal/routing/helpers.go
+++ internal/routing/helpers.go
@@ -2,21 +2,6 @@ package routing
 
 import "ride-home-router/internal/models"
 
-func insertAt(stops []*models.Participant, p *models.Participant, pos int) []*models.Participant {
-	result := make([]*models.Participant, len(stops)+1)
-	copy(result[:pos], stops[:pos])
-	result[pos] = p
-	copy(result[pos+1:], stops[pos:])
-	return result
-}
-
-func removeAt(stops []*models.Participant, pos int) []*models.Participant {
-	result := make([]*models.Participant, len(stops)-1)
-	copy(result[:pos], stops[:pos])
-	copy(result[pos:], stops[pos+1:])
-	return result
-}
-
 func removeRange(stops []*models.Participant, start, end int) []*models.Participant {
 	result := make([]*models.Participant, 0, len(stops)-(end-start))
 	result = append(result, stops[:start]...)
